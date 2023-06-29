84d1dc326303000e060365d3bb901da7d954b915
Jaden Falbee <j.falbee04@gmail.com>
2023-06-28 21:50:00 -0400

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavorite = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavorite = 'not changing it, bob still sucks.';
+}
